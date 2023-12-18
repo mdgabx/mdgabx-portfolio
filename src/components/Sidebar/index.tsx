afd7bc1c2f333032c7bc371ce8a6d80ea6bdfd11
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-12-18 21:09:08 +0800

    add: icons

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,16 +1,51 @@
 import { Link, NavLink } from 'react-router-dom';
 import wdbsaLogo from '../../assets/images/wdbsaLogo.png'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+ 
 const Sidebar:React.FC = () => (
     <div className="bg-black">
         <Link className="logo"  to="/">
             <img src={wdbsaLogo} alt="wdbsa logo" width="150" height="150"/>
         </Link>
-        <nav>
+        <nav className="flex flex-col items-center justify-center gap-5">
             <NavLink to="/">
-                
+                <FontAwesomeIcon
+                    icon={faHome} 
+                    color="#4d4d4e" 
+                    style={{ height: '24px' }} 
+                    className="hover:text-green-500 trasition duration-300 ease-in-out"
+                 />
+            </NavLink>
+            <NavLink to="/about">
+                <FontAwesomeIcon 
+                    icon={faUser} 
+                    color="#4d4d4e" 
+                    style={{ height: '24px' }}
+                    className="hover:text-green-500 trasition duration-300 ease-in-out"
+                />
+            </NavLink>
+            <NavLink to="/contact">
+                <FontAwesomeIcon 
+                    icon={faEnvelope} 
+                    color="#4d4d4e" 
+                    style={{ height: '24px' }} 
+                    className="hover:text-green-500 trasition duration-300 ease-in-out"
+                    />
             </NavLink>
         </nav>
+
+        <ul className="">
+            <li>
+                <a>
+                    <FontAwesomeIcon 
+                        icon={faLinkedin} 
+                        color="#4d4d4e" 
+                    />
+                </a>
+            </li>
+        </ul>
     </div>
 
 )
