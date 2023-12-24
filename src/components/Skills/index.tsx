f34fbc613b71ec8156ddf60679613859dcb94551
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-12-24 17:04:25 +0800

    add: grid layout

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -24,13 +24,13 @@ const Skills = () => {
     }, [])
     
     return (
-        <div className="w-3/4 flex flex-col mx-auto items-align justify-center">
+        <div className="w-3/4 flex flex-col mx-auto items-center justify-center">
             <div className="flex flex-col bg-white shadow-md shadow-black my-10 p-5 border-2 rounded-lg">
                 <h2 className="text-2xl font-poppins border-b border-gray-300 py-2">Technologies and Frameworks</h2>
-                <div>
+                <div className="p-2 md:p-10 grid grid-cols-2 md:grid-cols-4 gap-4">
                 {Array.isArray(skills) && skills.length > 0 ? (
                     skills.map((skill) => (
-                    <div key={skill.id} className="font-quicksand">
+                    <div key={skill.id} className="flex font-quicksand px-4">
                         {skill.name}
                     </div>
                     ))
