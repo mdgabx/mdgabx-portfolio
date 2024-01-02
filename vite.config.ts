d817bf240ba7e5058f17c451cf8921ca90e3c68f
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-01-02 08:59:16 +0800

    fix: deploy gh-pahes

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-
-  ],
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [
